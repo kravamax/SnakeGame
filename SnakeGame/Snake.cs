0dc43d4481c6b21e90da2853865e002c597fcf80
Maxim Kravchenko <maks.cross@gmail.com>
2020-10-05 21:42:21 +0300

    Add project files.

---
+++ SnakeGame/Snake.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Snake obj = new Snake();
+            obj.Show();
+            Console.ReadKey();
+        }
+
+    }
+    class Snake
+    {
+        public Snake() { }
+        public void Show()
+        {
+            Console.WriteLine("Hello, it's new game");
+        }
+    }
+}

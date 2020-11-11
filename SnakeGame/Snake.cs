122b37bb81cafc129728df411b2c772c570682c8
Maxim Kravchenko <maks.cross@gmail.com>
2020-11-11 22:36:50 +0200

    Added difficult levels, menu

--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 using System.Diagnostics;
 using System.Threading;             // класс запускает метод Loop
 using System.Collections.Generic;   //обобщенные или типизированные классы коллекций (IEnumerable <T> name = new IEnumerable <T>() {};)
@@ -19,36 +20,20 @@ namespace SnakeGame
         static int scores = 0;
         static int level = 0;
         static int snakeSpeed = 150;
-        static string playerName;
-        static Stopwatch stopWatch = new Stopwatch();
-        static void Main(string[] args)
+        static readonly Stopwatch stopWatch = new Stopwatch();
+        static void Main()
         {
             
-            Console.SetWindowSize(x + 1, y + 1);        // устанавливает размер окна консоли
-            Console.SetBufferSize(x + 1, y + 1);        // устанавливает размер буфера (убирает панель прокрутки)
-            Console.CursorVisible = false;              // скрываем курсор
+            SetWindowSize(x + 1, y + 1);        // устанавливает размер окна консоли
+            SetBufferSize(x + 1, y + 1);        // устанавливает размер буфера (убирает панель прокрутки)
+            CursorVisible = false;              // скрываем курсор                                          
 
-            AskName();                                  // функция регистрирует игрока
+            TittlePage();
 
-            walls = new Walls(x, y, '.');            
+            GameMenu();
 
-            snake = new Snake(x / 2, y / 3, 3);         // создаем объект змейки в центре поля и в длину 3 элемента
-                        
-            stopWatch.Start();                          // включаем секундомер
-
-            foodFactory = new FoodFactory(x, y, 'o');   // инициализация объекта еды координатами и символом
-            foodFactory.CreateFood();                   // добавляет еду для змейки
-
-            time = new Timer(Loop, null, 0, snakeSpeed);            
-
-            while (true)
-            {
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey(true);
-                    snake.Rotation(key.Key);
-                }
-            }
+            StartGame();
+            
         }
         static void Loop(object obj)
         {
@@ -58,7 +43,7 @@ namespace SnakeGame
                 time.Change(0, Timeout.Infinite);
                 GameOver();
             }
-            else if (snake.Eat(foodFactory.food))
+            else if (snake.Eat(foodFactory.Food))
             {
                 foodFactory.CreateFood();
                 scores++;
@@ -72,34 +57,119 @@ namespace SnakeGame
             else 
             { 
                 snake.Move();
-                Console.SetCursorPosition(0, 28);
+                SetCursorPosition(0, 28);
                 GameInfo();                                                             // вывод игрового табло с инфой
             }                
         }
-        static void AskName()
+        static void GameMenu()
         {
-            Console.Write("Enter your name: ");
-            playerName = Console.ReadLine();
+            int selectedMenuClass = ConsoleHelper.MultipleChoice(true, "start", "statistics","read rules", "exit");
+
+            switch (selectedMenuClass)
+            {
+                case 0: { Clear(); DificultyLevel(); break; }
+                case 1: { GameMenu(); /*GameStatistics();*/ break; }
+                case 2: { GameRules(); break; }
+                case 3: { GameOver(); time.Change(0, Timeout.Infinite); break; }
+            }
+            //Clear();
+        }
+        public static void GameRules()
+        {
+            string text = @"    Snake game - the main task of the game is to eat fruits
+
+            that appear on the field and move away from all obstacles.
+
+            The player maneuvers a growing snake, if the snake crashes 
+
+            into a wall or into itself - the game ends.
+
+
+                                      Goog game!";
+            Clear();
+            SetCursorPosition(15, 13);
+            WriteLine(text);
+            ReadKey();
+            GameMenu();
         }
         static void GameOver()
         {
             stopWatch.Stop();
-            Console.Clear();
-            Console.SetCursorPosition(37, 18);
-            Console.WriteLine("GAME OVER, " + playerName);
-            Console.SetCursorPosition(13, 28);
-            Console.WriteLine("Your scores: " + scores);
-            Console.SetCursorPosition(43, 28);
-            Console.Write("\t\tTime: {0:hh\\:mm\\:ss\\.ff}", stopWatch.Elapsed);
-            Console.ReadKey();
+            Clear();
+            SetCursorPosition(37, 18);
+            WriteLine("GAME OVER, " + "Noname");
+            SetCursorPosition(13, 28);
+            WriteLine("Your scores: " + scores);
+            SetCursorPosition(43, 28);
+            Write("\t\tTime: {0:mm\\:ss\\.ff}", stopWatch.Elapsed);
+            ReadKey();
         }
         static void GameInfo()
         {
-            Console.Write("\n\n\n\tPlayer: " + playerName);
-            Console.Write("\t\t\t\t\tLevel: " + level);
-            Console.Write("\n\n\n\tScores: " + scores);
-            Console.Write("\t\t\t\t\tTime:  {0:hh\\:mm\\:ss\\.ff}", stopWatch.Elapsed);
+            Write("\n\n\n\tPlayer: " + "Noname");
+            Write("\t\t\t\t\tLevel: " + level);
+            Write("\n\n\n\tScores: " + scores);
+            Write("\t\t\t\t\tTime:  {0:mm\\:ss\\.ff}", stopWatch.Elapsed);
         }
+        public static void TittlePage()
+        {
+            ForegroundColor = ConsoleColor.Green;
+            string SnakePicture = @"
+                           /^\/^\
+                         _|__|  O|
+                \/     /~     \_/ \
+                 \____|__________/  \
+                        \_______      \
+                                `\     \                 \
+                                  |     |                  \
+                                 /      /                    \
+                                /     /                       \\
+                              /      /                         \ \
+                             /     /                            \  \
+                           /     /             _----_            \   \
+                          /     /           _-~      ~-_         |   |
+                         (      (        _-~    _--_    ~-_     _/   |
+                          \      ~-____-~    _-~    ~-_    ~-_-~    /
+                            ~-_           _-~          ~-_       _-~
+                               ~--______-~                ~-___-~              
+                            ";            
+            Write(SnakePicture + "\n");
+            ResetColor();         
+        }
+        public static void DificultyLevel()
+        {
+            Clear();
+            WriteLine("Dificulty level: ");
+            int selectedDificultyClass = ConsoleHelper.MultipleChoice(true, "ease", "normal", "hard");
+
+            switch (selectedDificultyClass)
+            {
+                case 0: { Clear(); walls = new Walls(x, y, '.'); break; }
+                case 1: { Clear(); walls = new Walls(x, y, '.', 70, 5, 10, 21); break; }
+                case 2: { Clear(); walls = new Walls(x, y, '.', 70, 5, 10, 21, 60, 12); break; }
+            }
+        }
+        static void StartGame()
+        {
+            snake = new Snake(x / 2, 3, 1);         // создаем объект змейки в центре поля и в длину 3 элемента
+
+            stopWatch.Start();                          // включаем секундомер
+
+            foodFactory = new FoodFactory(x, y, 'o');   // инициализация объекта еды координатами и символом
+            foodFactory.CreateFood();                   // добавляет еду для змейки
+
+            time = new Timer(Loop, null, 0, snakeSpeed);
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    snake.Rotation(key.Key);
+                }
+            }
+        }
+
     }
     struct Point
     {
@@ -123,14 +193,14 @@ namespace SnakeGame
         }
         private void DrawPoint(char _ch)            //метод рисует символы границ
         {
-            Console.SetCursorPosition(x, y);        // получает позицию курсора
-            Console.Write(_ch);                     // выводит символ
+            SetCursorPosition(x, y);        // получает позицию курсора
+            Write(_ch);                     // выводит символ
         }
     }
     class Walls                                     // отрисовка стен
     {
-        private char ch;
-        private List<Point> wall = new List<Point>();
+        private readonly char ch;
+        private readonly List<Point> wall = new List<Point>();
 
         public Walls(int x, int y, char ch)         // конструктор принимает 3 параметра для отображения всех 4 стен заданным символом
         {
@@ -140,9 +210,20 @@ namespace SnakeGame
             DrawVertical(0, y);                     //левая граница
             DrawVertical(x, y);                     //правая граница
         }
-
-        private void DrawHorizontal(int x, int y)       // метод рисует горизонтальные стены
+        public Walls(int x, int y, char ch, int x2, int y2, int x3, int y3) : this(x, y, ch)        // уровень normal
         {
+            DrawHorizontalUp(x2, y2);
+            DrawHorizontalDown(x2, y2);
+            DrawVerticalLeft(x3, y3);
+            DrawVerticalRight(x3, y3);
+        }
+        public Walls(int x, int y, char ch, int x2, int y2, int x3, int y3, int x4, int y4) : this(x, y, ch, x2, y2, x3, y3)        // уровень HARD
+        {
+            DrawHorizontalCenter(x4, y4);
+            DrawVerticalCenter(x4, y4);
+        }
+        private void DrawHorizontal(int x, int y)       // метод рисует горизонтальные стены
+        {            
             for (int i = 0; i < x; i++)
             {
                 Point p = (i, y, ch);
@@ -155,6 +236,64 @@ namespace SnakeGame
             for (int i = 0; i < y - 10; i++)
             {
                 Point p = (x, i, ch);
+                p.Draw();
+                wall.Add(p);
+            }
+        }
+        private void DrawHorizontalUp(int x2, int y2)       // метод рисует горизонтальные внутренние верхние стены
+        {
+            for (int i = 10; i < x2; i++)
+            {
+                if (i > 20 && i < 60) continue;
+                Point p = (i, y2, ch);
+                p.Draw();
+                wall.Add(p);
+            }
+        }
+        private void DrawHorizontalDown(int x2, int y2)       // метод рисует горизонтальные внутренние нижние стены
+        {
+            for (int i = 10; i < x2; i++)
+            {
+                if (i > 20 && i < 60) continue;
+                Point p = (i, y2+15, ch);
+                p.Draw();
+                wall.Add(p);
+            }
+        }
+        private void DrawVerticalLeft(int x3, int y3)         // метод рисует вертикальные правые внутренние стены
+        {
+            for (int i = 5; i < y3; i++)
+            {
+                if (i > 10 && i < 15) continue;
+                Point p = (x3, i, ch);
+                p.Draw();
+                wall.Add(p);
+            }
+        }
+        private void DrawVerticalRight(int x3, int y3)         // метод рисует вертикальные левые внутренние стены
+        {
+            for (int i = 5; i < y3; i++)
+            {
+                if (i > 10 && i < 15) continue;
+                Point p = (x3+60, i, ch);
+                p.Draw();
+                wall.Add(p);
+            }
+        }
+        private void DrawHorizontalCenter(int x4, int y4)       // метод рисует горизонтальные внутренние верхние стены
+        {
+            for (int i = 20; i < x4; i++)
+            {
+                Point p = (i, y4, ch);
+                p.Draw();
+                wall.Add(p);
+            }
+        }
+        private void DrawVerticalCenter(int x4, int y4)         // метод рисует вертикальные правые внутренние стены
+        {
+            for (int i = 5; i < y4+9; i++)
+            {
+                Point p = (x4-20, i, ch);
                 p.Draw();
                 wall.Add(p);
             }
@@ -174,10 +313,10 @@ namespace SnakeGame
     }
     class Snake
     {
-        private List<Point> snake;                                              // список точек тела змеи
+        private readonly List<Point> snake;                                              // список точек тела змеи
 
         private Direction direction;                                            // переменная наших перечислений
-        private int step = 1;                                                   // переменная величины шага            
+        private readonly int step = 1;                                                   // переменная величины шага            
         private Point tail;                                                     // переменная для хвоста типа Point 
         private Point head;                                                     // переменная для головы типа Point                        
         bool rotate = true;
@@ -198,6 +337,7 @@ namespace SnakeGame
         {
             Console.ForegroundColor = ConsoleColor.Green;        // раскрас тела змейки   
             head = GetNextPoint();                               // голова принимает значение следующей точки координат
+            
             snake.Add(head);                                     // добавление символа головы
 
             tail = snake.First();                                // хвост принимает первый элемент последовательности
@@ -262,11 +402,11 @@ namespace SnakeGame
     }
     class FoodFactory                                   // генерация еды
     {
-        int x, y;
-        char ch;
-        public Point food { get; private set; }
+        readonly int x, y;
+        readonly char ch;
+        public Point Food { get; private set; }
 
-        Random random = new Random();
+        readonly Random random = new Random();
 
         public FoodFactory(int x, int y, char ch)       // Использовать лямбада-функции?
         {
@@ -277,9 +417,68 @@ namespace SnakeGame
 
         public void CreateFood()                                                // рисует еду в рандомных точках
         {
-            food = (random.Next(1, 79), random.Next(1, 25), ch);
+            Food = (random.Next(1, 79), random.Next(1, 25), ch);
             Console.ForegroundColor = ConsoleColor.Red;                         // меняем цвет еды
-            food.Draw();
+            Food.Draw();
         }
     }
+    public class ConsoleHelper
+    {
+        public static int MultipleChoice(bool canCancel, params string[] options)
+        {
+            int optionsPerLine = options.Length;
+
+            int currentSelection = 0;
+
+            ConsoleKey key;
+
+            CursorVisible = false;
+           
+            do
+            {
+                Clear();
+                Game.TittlePage();
+                for (int i = 0; i < options.Length; i++)
+                {
+                    if (i == currentSelection)
+                        ForegroundColor = ConsoleColor.Red;
+
+                    WriteLine("\t\t\t\t\t" + options[i]);
+                    ResetColor();
+                }
+
+                key = ReadKey(true).Key;                
+
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        {
+                            if (currentSelection > 0)
+                                currentSelection--;
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            if (currentSelection < optionsPerLine - 1)
+                                currentSelection++;
+                            break;
+
+                        }
+                    case ConsoleKey.Escape:
+                        {
+                            if (canCancel)
+                                return -1;
+                            break;
+                        }
+                }
+            } while (key != ConsoleKey.Enter);
+
+            CursorVisible = true;
+
+            return currentSelection;
+        }
+    }
+
+
+
 }
